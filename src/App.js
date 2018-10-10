8fc885ea765bec530e0355da1300b357831d73ad
Admin User <gtatrinidad@hotmail.com>
2018-10-10 09:50:14 -0400

    First push, firebase db connection testing

--- src/App.js
+++ src/App.js
@@ -2,14 +2,67 @@ import React, { Component } from 'react';
 import './App.css';
 import ToolbarTop from "./ToolbarTop/ToolbarTop";
 import SensorCard from "./SensorCard/SensorCard";
+import fire from './fire';
 
 class App extends Component {
+
+    constructor(){
+        super();
+
+        this.dataRef = fire.database().ref("systems").child('pi-1');
+
+        this.state = {
+            sensors: [],
+            device: ""
+        }
+    }
+
+    updateData(e){
+        e.preventDefault(); // <- prevent form submit from reloading the page
+        /* Send the message to Firebase */
+        this.dataRef.child('conc').child('avg').set( parseFloat(this.inputEl.value));
+        this.inputEl.value = ''; // <- clear the input
+    }
+
+    componentDidMount(){
+        //get some device here
+        var device = "pi-1";
+        this.setState({
+            device: device
+        });
+        //
+
+        var sensorData = [];
+        var count = 0;
+        this.dataRef.on("child_added", snap => {
+            var status = snap.child("status").val();
+
+            if(status){
+                sensorData[count] = {};
+                snap.forEach(function(val){
+                    sensorData[count][val.key] = val.val();
+                });
+                sensorData[count]['id'] = snap.key;
+                count++;
+            }
+            this.setState({
+                sensors: sensorData
+            });
+        });
+    }
 
     render() {
         return (
             <div className="App">
                 <ToolbarTop />
-                <SensorCard />
+                <SensorCard sensors={this.state.sensors} device={this.state.device}/>
+                <div>
+                    <form onSubmit={this.updateData.bind(this)}>
+                        <label>Update conc avg value</label>
+                        <input type="text" ref={ el => this.inputEl = el }/>
+                        <input type="submit"/>
+                    </form>
+                </div>
             </div>
         );
         //   return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'This might work...'));
--- src/SensorCard/SensorCard.js
+++ src/SensorCard/SensorCard.js
@@ -32,13 +32,13 @@ function sensorCard(props) {
         <Card className={classes.card}>
             <CardContent className={classes.innerContent}>
                 <Typography variant="h5" component="h2">
-                    Monitoring Unit 001
+                    Monitoring Unit: {props.device}
                 </Typography>
                 <Typography className={classes.subtitle} color="textSecondary">
                     (Values are updated every 2 minutes)
                 </Typography>
                 <Divider />
-                <DataTable />
+                <DataTable sensors={props.sensors}/>
             </CardContent>
         </Card>
     );
