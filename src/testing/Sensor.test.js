26e6d6beeec5ec6156a7c8451e397fb92ae355c5
Qarun Qadir Bissoondial <qarun80@hotmail.com>
2018-10-29 13:06:33 -0400

    - Added unit tests to the Sensor Component - Added TODOs in Dashboard.js

---
+++ src/testing/Sensor.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Sensor from '../components/sensor/Sensor';
+
+
+
+
+
+describe('Sensor Component', () => {
+    let shallow_render;
+
+    beforeEach(() => {
+        shallow_render = shallow(<Sensor/>);
+    });
+
+    it('-> should render without throwing any errors', () => {
+        console.log(shallow_render.debug());
+        expect(shallow_render.find('.sensorCard').exists()).toBe(true);
+    });
+
+    it('-> should render all other UI elements properly', () => {
+        expect(shallow_render.find('tr').length).toEqual(4);
+        expect(shallow_render.find('.justify-content-center').exists()).toBe(true);
+    });
+
+    it('-> should display all props on screen', () => {
+        let props = {
+            sensorName: true,
+            key: "",
+            min: 25,
+            max: {},
+            avg: "5",
+            current: "7",
+            device: "test123"
+        };
+
+        shallow_render = shallow(<Sensor {...props} />);
+        expect(shallow_render.find('#current').text()).toEqual(props.current);
+        expect(shallow_render.find('#avg').text()).toEqual(props.avg);
+    })
+});
