8d85720293bb70ef6908dd0d4857e0e5508a904a
Qarun Qadir Bissoondial <qarun80@hotmail.com>
2018-11-29 23:34:21 -0400

    - Added documentation to SignOut.js

--- src/components/auth/SignOut.js
+++ src/components/auth/SignOut.js
@@ -1,9 +1,15 @@
 import { Component } from 'react';
 import firebase from '../../fire';
 
-
+/**
+ * @class SignOut
+ */
 class SignOut extends Component {
 
+    /**
+     * @memberOf SignOut
+     * @desc Responsible for signing a user out of the system. The rest of the application is also notified
+     */
     componentDidMount(){
         firebase.auth().signOut().then(() => {
             this.props.history.push("/");
